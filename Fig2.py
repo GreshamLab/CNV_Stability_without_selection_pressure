26f50e76e9367d76867d98afd2262d5e175fc943
Titir De <td2101@nyu.edu>
2026-04-14 13:45:47 -0400

    Revise Fig2.py

--- Fig2.py
+++ Fig2.py
@@ -7,12 +7,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as stats
 from matplotlib.gridspec import GridSpec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 #--------------------------------------------
 
 ### Figure 2 
 
-fig = plt.figure(figsize=(6,7),layout="constrained")
+fig = plt.figure(figsize=(6.5,7),layout="constrained")
 
 gs = GridSpec(4, 2, figure=fig)
 ax1 = fig.add_subplot(gs[0,0])
@@ -24,12 +25,12 @@ ax4 = fig.add_subplot(gs[2:4,0:2])
 
 ax = ax1
 
-df = pd.read_csv('Pairwise_Comps_III_Stats_Python.CSV')
+df = pd.read_csv('Pairwise_III_edited_new.CSV')
 
-Strain_names = ['2543_1','2543_2','2543_3']
+Strain_names = ['2750_1','2750_2','2750_3']
 
 color_array = ['navy','deepskyblue','blue']
-linestyle_array = ['o:','s:','^:']
+linestyle_array = ['^:','o:','s:']
 
 for j in range(len(Strain_names)):
     gen_names = ['Gen_0','Gen_2_30C','Gen_5_30C','Gen_8_30C','Gen_12_30C','Gen_15_30C'] 
@@ -40,7 +41,8 @@ for j in range(len(Strain_names)):
     ax.plot(gen,(100-val), linestyle_array[j], color=color_array[j],ms=4)
 
 ax.set_ylabel('Abundance of \n strain (percent)')
-ax.set_ylim(15,55)
+ax.set_ylim(15,80)
+ax.text(4.5,70,'3 replicates of GM_13')
 
 
 #-------------
@@ -48,11 +50,11 @@ ax.set_ylim(15,55)
 ax = ax2
 
 color_array = ['navy','deepskyblue','blue','navy','deepskyblue','blue','navy','deepskyblue','blue']
-point_styles = ['o','s','^','o','s','^','o','s','^']
+point_styles = ['^','o','s','^','o','s','^','o','s']
 
-Strain ='DGY2543'
+Strain ='DGY2750'
 
-Strain_names = ['2543_1','2543_2','2543_3'] 
+Strain_names = ['2750_1','2750_2','2750_3'] 
 Gen_values = np.array([0,2,5,8,12,15,0,2,5,8,12,15,0,2,5,8,12,15])
 
 val_combine_30 = np.array([])    
@@ -72,9 +74,11 @@ for k in range(len(Strain_names)):
     ax.plot(gen,Ln_adj,point_styles[k],c=color_array[k],alpha=1.0,ms=4)
 
 slope, intercept, r_value, p_value, std_err = stats.linregress(Gen_values, val_combine_30)
+print(slope)
+print(std_err)
 
 ax.plot(Gen_values, intercept + slope*Gen_values, c='royalblue')
-ax.text(4,-0.3, "Slope = -0.065"'\n'"95% C.I. = -0.08, -0.05")
+ax.text(5.3,-0.4, "Slope = -0.086"'\n'"95% C.I.: (-0.05, -0.12)")
 
 ax.set_xlabel('Generations')
 ax.set_ylabel('ln (Strain/Ancestor)')
@@ -83,19 +87,20 @@ ax.set_ylabel('ln (Strain/Ancestor)')
 
 ax = ax3
 
-df1 = pd.read_csv('Fitness_vs_CNVsize.CSV')
+df1 = pd.read_csv('GaschFitnessCost_vs_Size.CSV')
 
-ax.errorbar(df1['Extra_bp']/1000, df1['Slope']+1, yerr=2*df1['Std_err'], fmt='o',ms=4,
-            color='black', ecolor='steelblue', capsize=3, lw=1.5)
+ax.errorbar(df1['CNV_cost'], df1['Slope']+1, yerr=2*df1['Std_err'], fmt='o',ms=4,
+            color='black', ecolor='grey', capsize=3, lw=1.5)
 
-ax.set_xlabel("CNV size (kilobases)")
+
+ax.set_xlabel("             Calculated CNV cost")
 ax.set_ylabel("Relative fitness")
-ax.set_ylim(0.92, 1.11)
-ax.set_xlim(10, 700)
-ax.set_xscale('log')
-ax.set_xticks([10,20,50,100,200,500],[10,20,50,100,200,500])
+ax.set_ylim(0.9, 1.11)
+ax.set_xlim(-82,1)
+ax.set_yticks([0.95,1.00,1.05,1.1])
 
-# Monte Carlo simulation to account for error bars
+
+#Monte Carlo simulation to account for error bars
 n_simulations = 10000
 simulated_corrs = []
 simulated_slope = []
@@ -103,20 +108,53 @@ simulated_slope = []
 x_arr = np.linspace(0,600,10)
 
 for _ in range(n_simulations):
-    simulated_slopes = np.random.normal(df1['Slope'], df1['Std_err'])  # Sample from normal dist using errors
-    sim_corr, _ = stats.pearsonr(np.log10(df1['Extra_bp']), simulated_slopes)
+    simulated_slopes = np.random.normal(df1['Slope'], df1['Std_err']) 
+    sim_corr, _ = stats.pearsonr(df1['CNV_cost'], simulated_slopes)
     simulated_corrs.append(sim_corr)
 
-# Compute mean and confidence intervals
+#Monte Carlo regression: compute slope + intercept for each simulation 
+sim_slopes_mc = []
+sim_intercepts_mc = []
+
+for _ in range(n_simulations):
+    y_sim = np.random.normal(df1['Slope']+1, df1['Std_err'])
+    slope_mc, intercept_mc = np.polyfit(df1['CNV_cost'], y_sim, 1)
+    sim_slopes_mc.append(slope_mc)
+    sim_intercepts_mc.append(intercept_mc)
+
+#Median regression line based on MC fits
+median_slope = np.median(sim_slopes_mc)
+median_intercept = np.median(sim_intercepts_mc)
+
+x_line = np.linspace(df1['CNV_cost'].min(), df1['CNV_cost'].max(), 200)
+y_line = median_slope * x_line + median_intercept
+
+ax.plot(x_line, y_line, '--', color='black', lw=1.5, zorder=1)
+
+#Linear regression confidence interval 
+
+y_all = np.zeros((n_simulations, len(x_line)))
+y_high = np.zeros_like(x_line)
+y_low = np.zeros_like(x_line)
+for i in range(n_simulations):
+    y_all[i,:] = sim_slopes_mc[i] * x_line + sim_intercepts_mc[i]
+
+for j in range(len(x_line)):
+    y_high[j] = np.percentile(y_all[:,j], 97.5)
+    y_low[j] = np.percentile(y_all[:,j], 2.5)
+ax.fill_between(x_line, y_high, y_low, color='grey', alpha=0.3, zorder=-1)
+
+#Compute mean and confidence intervals
 corr_mean = np.mean(simulated_corrs)
 lower, upper = np.percentile(simulated_corrs, [2.5, 97.5])
 
-print(f"Mean Pearson correlation: {corr_mean:.3f}")
-print(f"95% CI: ({lower:.3f}, {upper:.3f})")
+#print(f"Mean Pearson correlation: {corr_mean:.3f}")
+#print(f"95% CI: ({lower:.3f}, {upper:.3f})")
 
-ax.plot([0,1000],[1,1],':k')
+ax.text(-80, 1.085, 'Mean Pearson correlation: 0.385\n95% C.I.: (0.153, 0.609)')
 
-ax.set_yticks([0.95,1.00,1.05,1.1])
+ax.annotate('', xy=(0.10, -0.135), xycoords='axes fraction', xytext=(0.3, -0.135), 
+            arrowprops=dict(arrowstyle="->", color='k'))
 
 #-------------
 
@@ -167,9 +205,8 @@ ax.plot([x_start_aneu, x_start_aneu, x_end_aneu, x_end_aneu],
 ax.text((x_start_aneu + x_end_aneu)/2, y_bracket + 0.015, 'Aneuploidies', 
         ha='center', va='top', fontsize=8.5)
 
-
-#plt.savefig('Fig2.pdf',bbox_inches='tight',pad_inches=0.5)
-#plt.savefig('Fig2.png',pad_inches=0.05)
+#plt.savefig('Fig2_revision2.pdf',bbox_inches='tight',pad_inches=0.5)
+#plt.savefig('Fig2.png',pad_inches=0.05, dpi=600)
 
 plt.show()
 
